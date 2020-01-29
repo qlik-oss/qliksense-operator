16237fa50fe0cb9e687abb480847ac844273e86f
Foysal Iqbal <mqb@qlik.com>
2020-01-29 14:58:13 -0500

    initial operator framework
    Signed-off-by: Foysal Iqbal <mqb@qlik.com>

---
+++ pkg/apis/qlik/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the qlik v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=qlik.com
+package v1
---
+++ pkg/apis/qlik/v1/qliksense_types.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	kapis "github.com/qlik-oss/k-apis/config"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// QliksenseSpec defines the desired state of Qliksense
+type QliksenseSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	kapis.CRSpec `json:",inline"`
+}
+
+// QliksenseStatus defines the observed state of Qliksense
+type QliksenseStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Qliksense is the Schema for the qliksenses API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=qliksenses,scope=Namespaced
+type Qliksense struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   QliksenseSpec   `json:"spec,omitempty"`
+	Status QliksenseStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// QliksenseList contains a list of Qliksense
+type QliksenseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Qliksense `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Qliksense{}, &QliksenseList{})
+}
